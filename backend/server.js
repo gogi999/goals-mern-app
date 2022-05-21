982cece936465a1278d30cc4e214d86cb087a9b2
gogi999 <g.petrovic9@gmail.com>
2022-05-21 16:58:32 +0200

    Initial REST API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
 import goalRouter from './routes/goal.routes.js';
+import connectDB from './config/db.js';
 import { errorHandler } from './middleware/error.middleware.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 const port = process.env.PORT || 5000;
